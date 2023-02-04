33d6f87291ffaa6435197c963553193a19d21adb
dimas_anton77 <dimasanton44@gmail.com>
2023-02-04 14:30:33 +0700

    [GREEN] ROUTING

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,27 @@ $router->group(['prefix' => 'auth'], function () use ($router) {
     ]);
 });
 
+$router->group(['middleware' => 'auth', 'prefix' => 'api'], function () use ($router) {
+    $router->get('/role', 'RoleController@index');
+    $router->get('/role/{id}', 'RoleController@show');
+    $router->post('/role', 'RoleController@create');
+    $router->put('/role/{id}', 'RoleController@update');
+    $router->delete('/role/{id}', 'RoleController@delete');
+
+    $router->get('/member', 'MemberController@index');
+    $router->get('/member/{id}', 'MemberController@show');
+    $router->post('/member', 'MemberController@create');
+    $router->put('/member/{id}', 'MemberController@update');
+    $router->delete('/member/{id}', 'MemberController@delete');
+
+    $router->get('/transaksi', 'TransaksiController@index');
+    $router->get('/transaksi/{id}', 'TransaksiController@show');
+    $router->post('/transaksi', 'TransaksiController@create');
+    $router->put('/transaksi/{id}', 'TransaksiController@update');
+    $router->delete('/transaksi/{id}', 'TransaksiController@delete');
+    
+    $router->get('/histori-transaksi', 'HistoriTransaksiController@index');
+
+    $router->get('/profile/{id}', 'ProfileController@show');
+    $router->put('/profile/{id}', 'ProfileController@update');
+});
